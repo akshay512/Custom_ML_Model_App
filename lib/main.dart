61d5bc3eacfdf55c5e01263572508a0694cdfcb4
akshay512 <43413961+akshay512@users.noreply.github.com>
2019-10-30 16:54:04 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,198 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
+
+void main() {
+  runApp(MyApp());
+}
+
+const String ssd = "SSD MobileNet";
+const String yolo = "Tiny YOLOv2";
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TfliteHome(),
+    );
+  }
+}
+
+class TfliteHome extends StatefulWidget {
+  @override
+  _TfliteHomeState createState() => _TfliteHomeState();
+}
+
+class _TfliteHomeState extends State<TfliteHome> {
+  String _model = ssd;
+  File _image;
+
+  double _imageWidth;
+  double _imageHeight;
+  bool _busy = false;
+  List _recognitions;
+
+  @override
+  void initState() {
+    super.initState();
+    _busy = true;
+
+    loadModel().then((val) {
+      setState(() {
+        _busy = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    Tflite.close();
+
+    try {
+      String response;
+      if (_model == yolo) {
+        response = await Tflite.loadModel(
+            model: "assets/tflite/yolov2_tiny.tflite",
+            labels: "assets/tflite/yolov2_tiny.txt");
+      } else {
+        response = await Tflite.loadModel(
+            model: "assets/tflite/ssd_mobilenet.tflite",
+            labels: "assets/tflite/ssd_mobilenet.txt");
+        print(response);
+      }
+    } on PlatformException {
+      print("Couldn't load Model");
+    }
+  }
+
+  selectImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+
+    setState(() {
+      _busy = true;
+    });
+
+    predictImage(image);
+  }
+
+  predictImage(File image) async {
+    if (image == null) return;
+
+    if (_model == yolo) {
+      await yoloV2Tiny(image);
+    } else {
+      await ssdMobileNet(image);
+    }
+
+    FileImage(image)
+        .resolve(ImageConfiguration())
+        .addListener((ImageStreamListener((ImageInfo info, bool _) {
+          setState(() {
+            _imageHeight = info.image.width.toDouble();
+            _imageWidth = info.image.height.toDouble();
+          });
+        })));
+
+    setState(() {
+      _image = image;
+      _busy = false;
+    });
+  }
+
+  yoloV2Tiny(File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path,
+        model: "YOLO",
+        threshold: 0.3,
+        imageMean: 0.0,
+        imageStd: 255,
+        numResultsPerClass: 1);
+
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+  ssdMobileNet(File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path, numResultsPerClass: 1);
+
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageWidth == null || _imageHeight == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageHeight * screen.width;
+
+    Color color = Colors.blue;
+
+    return _recognitions.map((re) {
+      return Positioned(
+        left: re["rect"]["x"] * factorX,
+        top: re["rect"]["y"] * factorY,
+        width: re["rect"]["w"] * factorX,
+        height: re["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: color,
+              width: 3,
+            ),
+          ),
+          child: Text(
+            "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+                background: Paint()..color = color,
+                fontSize: 15,
+                color: Colors.white),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      top: 0.0,
+      left: 0.0,
+      width: size.width,
+      child: _image == null ? Text('No image Selected') : Image.file(_image),
+    ));
+
+    stackChildren.addAll(renderBoxes(size));
+
+    if (_busy) {
+      stackChildren.add(Center(
+        child: CircularProgressIndicator(),
+      ));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TFLite Demo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.image),
+        onPressed: selectImage,
+        tooltip: 'Pick a Image',
+      ),
+      body: Stack(
+        children: stackChildren,
+      ),
+    );
+  }
+}
